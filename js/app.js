b0536f84111a6b77a9553fda5bea405a6ecfe14d
Hubert Nguyen <hubertanguyen@gmail.com>
2015-03-20 22:06:03 -0700

    rendered album song/name dynamic with route

--- js/app.js
+++ js/app.js
@@ -3,12 +3,22 @@
 	window.App = Ember.Application.create();
 })();
 
-// Creating route, which wills supply model to the application template
-
+// Creating route, which will supply model to the application template
 App.ApplicationRoute = Ember.Route.extend({
-	model: function() {
-		return {
-			name: "hubert"		
+	model:function() {
+		// you want to return an array of objects
+		var arrayObjs = [];
+		// loop through fixture data
+		for (var i = 0; i < App.ALBUM_FIXTURES.length; i++) {
+			// push objects with artist name and song into array
+			arrayObjs.push(
+				{
+					albumName: App.ALBUM_FIXTURES[i].name,
+					albumArtist: App.ALBUM_FIXTURES[i].artist
+				}
+			)
 		}
+		// return array of objcts with album name and artist
+		return arrayObjs;
 	}
 })
