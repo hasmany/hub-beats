4b4db149c48cba497c16269c69849f5c8f807086
Hubert Nguyen <hubertanguyen@gmail.com>
2015-03-20 21:52:17 -0700

    Creation of appliation route for main template

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@
 	"use strict";
 	window.App = Ember.Application.create();
 })();
+
+// Creating route, which wills supply model to the application template
+
+App.ApplicationRoute = Ember.Route.extend({
+	model: function() {
+		return {
+			name: "hubert"		
+		}
+	}
+})
