bcc1c310b375a81759c69bcf490ea06585c27d39
Hubert Nguyen <hubertanguyen@gmail.com>
2015-04-10 16:37:45 -0500

    Seprated index template from application

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
 (function(){
 	"use strict";
+
+  // Application Instance
 	window.App = Ember.Application.create();
+
+  // Main Application Route
+  App.ApplicationRoute = Ember.Route.extend({
+
+  });
+
+  // Map Dynamic Routes
+  App.Router.map(function(){
+    this.resource('album',{path: 'album/:album_id'});
+  });
+
+  // Index Route
+  App.IndexRoute = Ember.Route.extend({
+     model: function(){
+      return App.ALBUM_FIXTURES;
+    }
+  });
+
+  //Album Route
+  App.AlbumRoute = Ember.Route.extend({
+    model: function(params) {
+      return App.ALBUM_FIXTURES.findProperty('id',params.album_id);
+    }
+  });
+
 })();
-
-// Creating route, which will supply model to the application template
-// App.ApplicationRoute = Ember.Route.extend({
-// 	model:function() {
-// 		// you want to return an array of objects
-// 		var arrayObjs = [];
-// 		// loop through fixture data
-// 		for (var i = 0; i < App.ALBUM_FIXTURES.length; i++) {
-// 			// push objects with artist name and song into array
-// 			arrayObjs.push(
-// 				{
-// 					albumName: App.ALBUM_FIXTURES[i].name,
-// 					albumArtist: App.ALBUM_FIXTURES[i].artist
-// 				}
-// 			)
-// 		}
-// 		// return array of objcts with album name and artist
-// 		return arrayObjs;
-// 	}
-// })
-
-// Simpler solution
-// look at properties in fixture
-// see that it is already an array of objects
-// see that you can use its properties in the template
-App.ApplicationRoute = Ember.Route.extend({
-	model: function(){
-		return App.ALBUM_FIXTURES;
-	
-	}
-});
